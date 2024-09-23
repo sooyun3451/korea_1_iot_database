f20e8d2278c8933ceaf411bc060a86b21ec533bc
sooyun3451 <sooyun020820@gmail.com>
2024-09-23 12:47:50 +0900

    20240923

---
+++ a_개발환경/helloSQL.sql
@@ -0,0 +1,42 @@
+SHOW DATABASES;
+
+--  MySQL Workbench 환경설정 
+
+-- 1. 주석 
+-- 주석 단축키 ) ctrl(commend) + /
+
+-- 1-1. 한줄 주석 
+-- 하이픈 2개를 연속으로 사용 
+# 샾 기호 1개를 사용 
+
+-- 1-2 여러줄 주석 
+/*
+슬래시 2개 사이에 별 2개를 사용 
+>> 어떤 문장이나 여러 줄을 작성해도 모두 주석으로 인정 
+*/
+
+# 2. 글자 크기 변경 
+# ctrl + 마우스 윌 위/아래 
+# Edit >> preferences >> Fonts & Colors
+
+# 3. sql 명령어 세팅 (대문자, 소문자)
+# 대소문자 구분 X
+# SQL 명령어는 관례적으로 대문자로 작성하는 것이 일반적 
+# Edit >> Format >> UPCASE(대문자) / lowercase(소문자) keywords 선택 
+
+# 4. sql 자동 줄바꿈(Word Wrap) 
+# 
+
+# 5. 다크 모드 설정 
+#
+
+# 6. sql 기본 단축키 
+# 파일 저장: ctrl(commend) + s
+# 한 줄 복사: ctrl(commend) + d 
+# 한 줄 삭제: ctrl(commend) + l(소문자 L)
+# 실행 단축키: 
+-- 마우스 커서가 위치하는 쿼리문을 실행: ctrl(commend) + enter 
+-- 마우스로 드래그한 영역의 쿼리문만을 실행: ctrl(commend) + shift + enter
+
+# 새 SQL 에디터 열기(새로운 파일 생성): ctrl(commend) + t
+# SQL 정렬: ctrl(commend) + b
